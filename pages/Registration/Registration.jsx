7fee8e2308402ed11c8d0a7ba142c866f5283dbf
lelaranja <leticiajlaranja@gmail.com>
2022-08-27 23:18:24 -0300

    feat: ajuste de css

--- pages/Registration/Registration.jsx
+++ pages/Registration/Registration.jsx
@@ -21,9 +21,11 @@ const Registration = () => {
 
   return (
     <div className={S.container}>
-      {view == "form1" && <FormOne onClick={handleViewScd} />}
-      {view == 'form2' && <FormTwo onClick={handleViewTrd} />}
-      {view == 'form3' && <FormThree />}
+      <h1>Formulário de cadastro</h1>
+      {/* {view == "form1" && <FormOne onClick={handleViewScd} />}
+      {view == "form2" && <FormTwo onClick={handleViewTrd} />}
+      {view == "form3" && <FormThree />} */}
+      <FormTwo />
     </div>
   );
 };
